9b401e8aec0c36cb5a6576e6cdadc1e37095ee1c
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-04-16 19:44:43 -0500

    created 'terms' and 'contact' modal

--- New Era Wavs/main.js
+++ New Era Wavs/main.js
@@ -1,30 +1,60 @@
 window.onload=function(){
     // Modal variables
-    var modal, aboutBtn, termsBtn, contactBtn, closeBtn;
-    modal = document.getElementById("aboutModal");
-    aboutBtn= document.querySelector("#btn-about");
+    var aboutModal,termsModal, contactModal, aboutBtn, termsBtn, contactBtn, closeBtn;
+    aboutModal = document.getElementById("aboutModal");
+    termsModal = document.getElementById("termsModal");
+    contactModal = document.getElementById("contactModal");
+    aboutBtn= document.getElementById("aboutBtn");
     termsBtn= document.getElementById("termsBtn");
     contactBtn= document.getElementById("contactBtn");
     closeBtn = document.querySelector(".closeBtn");
 
-    //  Click Listner
-    function openModal(){
-        modal.style.display='block';
+    //  Modal functions
+    function openAboutModal(){
+        aboutModal.style.display='block';  
     };
 
-    function closeModal(){
-        modal.style.display='none';
+    function closeAboutModal(){
+        aboutModal.style.display='none';
     };
 
-    aboutBtn.addEventListener('click', openModal);
-    closeBtn.addEventListener('click', closeModal);
+    function openTermsModal(){
+        termsModal.style.display='block';
+    };
+
+    function closeTermsModal(){
+        termsModal.style.display='none';
+    };
+
+    function openContactModal(){
+        contactModal.style.display='block';
+    };
+
+    function closeContactModal(){
+        contactModal.style.display='none';
+    };
+
+    aboutBtn.addEventListener('click', openAboutModal);
+    closeBtn.addEventListener('click', closeAboutModal);
+
+    termsBtn.addEventListener('click', openTermsModal);
+    closeBtn.addEventListener('click', closeTermsModal);
+
+    contactBtn.addEventListener('click', openContactModal);
+    closeBtn.addEventListener('click', closeContactModal);
+    
 
     // Outside click listner
     window.addEventListener('click',outsideClick);
 
     function outsideClick(e){
-        if(e.target==modal){
-            modal.style.display='none';
+        if(e.target==aboutModal){
+            aboutModal.style.display='none';
+        }
+        else if(e.target==termsModal){
+            termsModal.style.display='none';
+        }else{
+            contactModal.style.display='none';
         }
     }
 
